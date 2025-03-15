3afc2e881ddc6cd1c65ef1e316ac591409847299
Juan David Montero <juan.montero@movisoftware.co>
2025-03-14 19:41:35 -0500

    se arreglan problemas

--- src/App.js
+++ src/App.js
@@ -9,24 +9,15 @@ import InfoBottomPage from "./components/infoBottomPage.jsx";
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <Navbar /> 
-      <div style={{paddingBottom:'50px '}}>
+      <Navbar />
+      <div style={{ paddingBottom: "50px " }}>
         <Routes>
           <Route path="/inmobiliaria_frontend" element={<Home />} />
           <Route path="/marketplace" element={<Marketplace />} />
           <Route path="/marketing" element={<Marketing />} />
         </Routes>
       </div>
-=======
-      <Navbar />
-      <Routes>
-        <Route path="/inmobiliaria_frontend" element={<Home />} />
-        <Route path="/marketplace" element={<Marketplace />} />
-        <Route path="/marketing" element={<Marketing />} />
-      </Routes>
       <InfoBottomPage />
->>>>>>> feature-sprint3
     </>
   );
 }
