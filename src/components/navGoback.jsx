9c43bcb5c547cb1d49ea07383c952452f84b4887
Juan David Montero <juan.montero@movisoftware.co>
2025-04-02 20:59:49 -0500

    ajustar redireccion navbar

--- src/components/navGoback.jsx
+++ src/components/navGoback.jsx
@@ -1,24 +1,22 @@
-import React from 'react'
-import "../styles/navGoback.css";
-import { FaArrowLeft } from "react-icons/fa6";
-import { useNavigate } from "react-router-dom";  // <-- Importar useNavigate
+import React from 'react';
+import '../styles/navGoback.css';
+import { FaArrowLeft } from 'react-icons/fa6';
+import { useNavigate } from 'react-router-dom'; // <-- Importar useNavigate
 
 const GoBack = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const irAMarket = () => {
+    navigate(`/inmobiliaria_frontend/#/marketplace`);
+  };
 
-    const irAMarket = () => {
-      navigate(`/inmobiliaria_frontend/marketplace`);
-    };
-
-    
   return (
-    <div className='ContainerNavGoBack'>
-        <button onClick={irAMarket}>
-          <FaArrowLeft />
-        </button>
+    <div className="ContainerNavGoBack">
+      <button onClick={irAMarket}>
+        <FaArrowLeft />
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default GoBack
+export default GoBack;
