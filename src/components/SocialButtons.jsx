f933caa487c94521256e98dcc31e27c79c169e98
jpalaciosd <juandiegopalaciosdelgado@gmail.com>
2025-04-17 16:47:50 -0500

    Update - Presentacion botones

--- src/components/SocialButtons.jsx
+++ src/components/SocialButtons.jsx
@@ -1,48 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { FaInstagram, FaRobot, FaFacebookF } from 'react-icons/fa';
 import { SiTiktok } from 'react-icons/si';
 import '../styles/socialButtons.css';
 
 const SocialButtons = () => {
-  const [temaClaro, setTemaClaro] = useState(false);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      entries => {
-        for (const entry of entries) {
-          if (entry.isIntersecting) {
-            const clase = entry.target.className;
-            if (clase.includes('seccion-clara')) {
-              setTemaClaro(true);
-            } else if (clase.includes('seccion-oscura')) {
-              setTemaClaro(false);
-            }
-          }
-        }
-      },
-      { threshold: 0.3 }
-    );
-
-    const secciones = document.querySelectorAll('.seccion-clara, .seccion-oscura');
-    secciones.forEach(seccion => observer.observe(seccion));
-
-    return () => observer.disconnect();
-  }, []);
-
   return (
-    <div className={`social-buttons ${temaClaro ? 'tema-claro' : 'tema-oscuro'}`}>
-      <a href="#bot" className="social-button">
+    <div className="social-buttons">
+      <a href="https://wa.link/y07kts" target="_blank" rel="noopener noreferrer" className="social-button" aria-label="Bot">
         <FaRobot />
       </a>
       
-      <a href="https://www.instagram.com/mariopaz.inmobiliaria" target="_blank" rel="noopener noreferrer" className="social-button">
+      <a href="https://www.instagram.com/mariopaz.inmobiliaria" target="_blank" rel="noopener noreferrer" className="social-button" aria-label="Instagram">
         <FaInstagram />
       </a>
       
-      <a href="https://www.facebook.com/share/1VMTSFoR94/   " target="_blank" rel="noopener noreferrer" className="social-button">
+      <a href="https://www.facebook.com/share/1VMTSFoR94/" target="_blank" rel="noopener noreferrer" className="social-button" aria-label="Facebook">
         <FaFacebookF />
       </a>
-      <a href="https://www.tiktok.com/@mariopazinmobiliaria?_t=ZS-8vQfnpCAhR2&_r=1" target="_blank" rel="noopener noreferrer" className="social-button">
+
+      <a href="https://www.tiktok.com/@mariopazinmobiliaria?_t=ZS-8vQfnpCAhR2&_r=1" target="_blank" rel="noopener noreferrer" className="social-button" aria-label="TikTok">
         <SiTiktok />
       </a>
     </div>
