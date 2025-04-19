fb8216c3fbe781fca668fc5152d27f5acb2b8b0c
jpalaciosd <juandiegopalaciosdelgado@gmail.com>
2025-04-19 18:22:20 -0500

    Update - video presentacion comprimido

--- src/components/Bienvenida.jsx
+++ src/components/Bienvenida.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../styles/bienvenida.css';
 import video1 from '../assets/MP.1.mp4';
-import { useNavigate } from 'react-router-dom'; // <-- Importar useNavigate
+import { useNavigate } from 'react-router-dom';
 
 const Bienvenida = () => {
   const navigate = useNavigate();
@@ -12,7 +12,16 @@ const Bienvenida = () => {
 
   return (
     <div className="bienvenida-container">
-      <video src={video1} className="video-background" autoPlay muted playsInline loop />
+      <video
+        src={video1}
+        className="video-background"
+        autoPlay
+        muted
+        loop
+        playsInline
+        preload="auto" // mejora el tiempo de arranque
+        poster="/images/fallback.jpg" // opcional: imagen mientras carga
+      />
       <div className="bienvenida-content">
         <div className="bienvenida-text">
           <h1>Descubre el hogar de tus sueños con Mario Paz inmobiliaria</h1>
